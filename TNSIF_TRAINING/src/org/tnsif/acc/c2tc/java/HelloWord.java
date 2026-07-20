f5f84c40769aa9242dcbcafce3950cd0dbdc86b0
SHASHANK KN <shashankkn.cs@gmail.com>
2026-07-20 09:44:52 +0530

    first commit

---
+++ TNSIF_TRAINING/src/org/tnsif/acc/c2tc/java/HelloWord.java
@@ -0,0 +1,11 @@
+package org.tnsif.acc.c2tc.java;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+		// TODO Auto-generated method stub
+
+	}
+
+}
