144ca982ee77c738a722be90556263248e37209d
SHASHANK KN <shashankkn.cs@gmail.com>
2026-07-20 10:05:40 +0530

    first commit

---
+++ TNSIF_TRAINING/src/org/tnsif/acc/c2tc/java/VariableDemo.java
@@ -0,0 +1,22 @@
+package org.tnsif.acc.c2tc.java;
+
+public class VariableDemo {
+	int price=10;
+	void print()
+	{
+		String msg="hello";
+		System.out.println(msg);
+		
+	}
+	static String message="hello students";
+
+	public static void main(String[] args) {
+		VariableDemo obj =new VariableDemo();
+		System.out.println(obj.price);
+		obj.print();
+		System.out.println(message);
+		// TODO Auto-generated method stub
+
+	}
+
+}
