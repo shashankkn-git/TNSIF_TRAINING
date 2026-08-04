350e560409bac05fd53201f0f06bc0f2da31f443
SHASHANK KN <shashankkn.cs@gmail.com>
2026-08-04 10:19:52 +0530

    demo11

--- TNSIF_TRAINING/src/org/tnsif/acc/c2tc/scannerclass_bufferreader/ScannerClassdemo.java
+++ TNSIF_TRAINING/src/org/tnsif/acc/c2tc/scannerclass_bufferreader/ScannerClassdemo.java
@@ -2,13 +2,26 @@ package org.tnsif.acc.c2tc.scannerclass_bufferreader;
 import java.util.Scanner;
 public class ScannerClassdemo {
 	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Enter your name");
-		 String name =  scan.nextLine();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+		 String name =  sc.nextLine();
 		 System.out.println("Enter your age");
-		    int age = scan.nextInt();
+		 int age = sc.nextInt();
+		 System.out.println("Enter your salary");
+		 double salary =sc.nextDouble();
+		 System.out.println("Enter your balance");
+		 float balance = sc.nextFloat();
+		 System.out.print("do you have other bank account: ");
+	     boolean value = sc.nextBoolean();
+		 System.out.print("Enter a character: ");
+         char ch = sc.next().charAt(1);
 		 System.out.println("Name of the User: "+name);
 		 System.out.println("Age of the User :"+age);
-		 scan.close();
+		 System.out.println("salry is :" + salary);
+		 System.out.println("balance is :"+balance);
+		 System.out.println("yes or no = " + value);
+         System.out.print("Enter a character: ");
+         System.out.println("You entered: " + ch);
+         sc.close();
 	}
 }
