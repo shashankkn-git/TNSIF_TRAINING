d01eafb2b0a31ee8bf94f0c271e8a23f840d86fc
SHASHANK KN <shashankkn.cs@gmail.com>
2026-08-05 10:44:01 +0530

    commit

---
+++ TNSIF_TRAINING/src/org/tnsif/acc/c2tc/senior_developer/KIA_Internal.java
@@ -0,0 +1,12 @@
+package org.tnsif.acc.c2tc.senior_developer;
+import org.tnsif.acc.c2tc.junior_developer.*;
+public class KIA_Internal {
+
+	public static void main(String[] args) {
+		KIA_Airbags airbag=new KIA_Airbags();
+		airbag.disp_airbags();
+		
+
+	}
+
+}
